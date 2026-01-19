9f92f15abf84e01f8688380f3c475186a4130dbd
lnco6 <helene.caux6@gmail.com>
2026-01-19 10:54:58 +0100

    ajout page js

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+// Menu burger, explications trouvées sur https://iridescent.dev/posts/web/creer-un-menu-hamburger
+
+let burger = document.getElementById("burger");
+let nav = document.getElementById("menuPrincipal");
+
+burger.onclick = function () {
+    if (nav.className === "open") {
+        nav.className = "";
+    } else {
+        nav.className = "open";
+    }
+};
